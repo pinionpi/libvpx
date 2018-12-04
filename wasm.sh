44562e48a62badf2381e248c0d1363e2c3e97b0a
Anton Khayrudinov <ankh@google.com>
2018-12-03 20:54:37 -0800

    Fixed some VPX compile flags.

--- wasm.sh
+++ wasm.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 emconfigure ./configure --target=generic-gnu \
-  --disable-multithread \
   --disable-runtime-cpu-detect \
   --disable-unit-tests \
   --disable-install-docs \
   --disable-examples \
-  --enable-realtime-only
+  --enable-realtime-only \
+  --enable-pic \
+  --enable-multi-res-encoding \
+  --enable-temporal-denoising \
 
 emmake make
 
-# Replace -O2 with --profiling --profiling-funcs -g4 and use Chrome's profiler.
 emcc -v \
-  -O2 \
+  -O3 --profiling --profiling-funcs -g4 \
   -s ALLOW_MEMORY_GROWTH=1 \
   libvpx.a -o libvpx.js
 
-ls -l --block-size K libvpx.*
+ls -l --block-size K *.{wasm,js}
